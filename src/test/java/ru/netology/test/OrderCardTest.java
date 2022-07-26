b62f798eb69a730529de77a8909fe66156464b95
dofpo <z-salawat91@mail.ru>
2022-07-26 18:35:31 +0500

    initial first commit

---
+++ src/test/java/ru/netology/test/OrderCardTest.java
@@ -0,0 +1,23 @@
+package ru.netology.test;
+
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class OrderCardTest {
+    @Test
+    void test() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Иванов Иван");
+        form.$("[data-test-id='phone'] input").setValue("+78003000600");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
